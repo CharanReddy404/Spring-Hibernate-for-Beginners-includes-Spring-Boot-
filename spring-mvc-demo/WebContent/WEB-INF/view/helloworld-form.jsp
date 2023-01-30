690827c12ff2015f8814bce97dc647067fb296e7
charanreddy404 <charanlucky6143@gmail.com>
2023-01-30 07:47:53 +0530

    requesting mapping

--- spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/helloworld-form.jsp
@@ -18,5 +18,14 @@
 		<input type="submit" />
 	</form>
 	
+	
+	<form action="processFormVersionThree" method="GET">
+		<input type="text" name="studentName"
+			placeholder="what's your name Version-3"/>
+		
+		<input type="submit" />
+	</form>
+	
+	
 </body>
 </html>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -37,6 +38,25 @@ public class HelloWorldController {
 		
 		//Create the message
 		String result = "Yo! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName , Model model) {
+		
+//		// read the request parameter from the HTML form
+//		String theName = request.getParameter("studentName");
+		
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		//Create the message
+		String result = "Yo Yo! My Friend " + theName;
 		
 		// add message to the model
 		model.addAttribute("message", result);
